13f4ee323beacd976dd9b03ad2a171c2f218c329
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-03-14 21:35:18 -0500

    fix favorites icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "../styles/navbar.module.css";
 import Lemonlogo from "../public/iconComponents/lemonlogo";
-import Favoritelogo from "../public/iconComponents/favoritelogo";
 import Link from "next/dist/client/link";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import { styled } from "@mui/styles";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const StyledBtn = styled(Button)({
   borderRadius: 40,
@@ -12,16 +12,26 @@ const StyledBtn = styled(Button)({
   padding: ".2em 1em .2em 1em",
 });
 
+const NavFavIcon = styled(FavoriteIcon)({
+  fontSize: 32,
+});
+
 const Navbar = () => {
   return (
     <div className={styles.navwrap}>
       <div className={styles.logowrap}>
-        <Lemonlogo />
+        <Link href="/" passHref>
+          <IconButton disableRipple>
+            <Lemonlogo />
+          </IconButton>
+        </Link>
       </div>
       <nav className={styles.nav}>
         <ul>
           <li>
-            <Favoritelogo />
+            <IconButton disableRipple sx={{ padding: 0 }}>
+              <NavFavIcon color="secondary" />
+            </IconButton>
           </li>
           <li>
             <Link href="/" passHref>
